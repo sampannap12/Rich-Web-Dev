722b26bb77831db74439fc7668d5a53e5c90ff63
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2022-11-07 15:39:21 +0000

    updated app.js

--- lab2/problem2/app.js
+++ lab2/problem2/app.js
@@ -1,6 +1,4 @@
 // To get the program running, open the apiQ2HTML.html file and go inspect element and then console. It should show the console logs
-
-
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json())
 .then(json => console.log(json.filter(titleLength))) //for title part
@@ -38,6 +36,6 @@ getPosts()
             {wordOcc[wordCount]++}
             else{wordOcc[wordCount]=1}
         }))
-        console.log(wordOcc)
+        console.log(wordOcc) //word count
     })
 
