47a977203e4b1dee757e917e3731bab4a60b5a02
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2022-11-07 10:38:59 +0000

    added word occurred function

--- lab2/problem2/app.js
+++ lab2/problem2/app.js
@@ -10,3 +10,28 @@ const titles = json.title
 const words = titles.split(" ")
 return words.length > 6
 }
+
+const getPosts = async() =>{
+    const getData = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await getData.json();
+    return data;
+}
+
+
+getPosts()
+.then (data => data.map((posts)=> {
+    postsBody=(posts.body)
+    return (postsBody)
+}))
+    .then(wordCount =>{
+        wordOcc=[]
+        words = wordCount
+        .map (wordCount =>wordCount.split(' ')
+        .map(wordCount=>{
+            if(wordCount in wordOcc)
+            {wordOcc[wordCount]++}
+            else{wordOcc[wordCount]=1}
+        }))
+        console.log(wordOcc)
+    })
+
