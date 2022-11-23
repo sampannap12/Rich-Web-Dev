3b11d68f855f56e504d3ef79354c0dc5c6111054
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2022-11-23 14:41:09 +0000

    added rxjs subscirption and updated script
    updated script with rxjs as well as update d script to have editable title and message

--- lab3/problem1/script.js
+++ lab3/problem1/script.js
@@ -1,83 +1,44 @@
-(function() {
-  
+function addNote() {
 
-    var app = {
-      noteEditor: document.getElementById('note-editor'),
-      noteEditorTitle: document.getElementById('note-editor-title'),
-      title: document.getElementById('title'),
-      message: document.getElementById('message'),
-      color: document.getElementById('color'),
-      addButton: document.getElementById('add-btn'),
-      deleteButton: document.querySelector('.delete'),
-      editButton: document.querySelector('.edit'),
-      notesSection: document.getElementById('notes-section'),
-      notes: document.getElementById('notes'),
+    let addButton = document.getElementById('add-btn');
+    let notes = document.getElementById('notes');
+
+      var li = document.createElement('li'),
+      deleteBtn = document.createElement('span'),
+      editBtn = document.createElement('span'),
+      titles = document.createElement('span'),
+      messages = document.createElement('span');
+      titles.className = 'note-title';
+      titles.innerHTML = document.getElementById('title').value;
+      titles.contentEditable ="true";
+      messages.className = 'note-message';
+      messages.innerHTML = document.getElementById('message').value;
+      messages.contentEditable="true";
+      li.className = document.getElementById('color').value;
       
-  
-      init: function() {
-        app.addButton.addEventListener('click', app.createNote);
-       
-      },
-      //Create function
-      
-      createNote: function() {
-        
-          var note = new Object();
-          note.title = app.title.value;
-          note.message = app.message.value;
-          note.color = app.color.value;
-          app.addNote(note);
-        },
-     //Add Function 
+      li.appendChild(deleteBtn);
+      li.appendChild(titles);
+      li.appendChild(messages);
+      li.appendChild(editBtn);
+      notes.prepend(li);
      
-      addNote: function(note) {
-        var li = document.createElement('li'),
-        deleteBtn = document.createElement('span'),
-        editBtn = document.createElement('span'),
-        title = document.createElement('span'),
-        message = document.createElement('span');
-        title.className = 'note-title';
-        title.innerHTML = note.title;
-        message.className = 'note-message';
-        message.innerHTML = note.message;
-        li.className = note.color;
-        
-        li.appendChild(deleteBtn);
-        li.appendChild(title);
-        li.appendChild(message);
-        li.appendChild(editBtn);
-  
-        deleteBtn.className = 'delete';
-        deleteBtn.innerHTML = 'Delete';
-        deleteBtn.addEventListener('click', app.deleteNote);
-  
-        editBtn.className = 'edit';
-        editBtn.innerHTML = 'Edit';
-        editBtn.addEventListener('click', app.editNote);
-        
-        app.notes.prepend(li);
-  
-        app.title.value = '';
-        app.message.value = '';
-  
-        
-      },
-      //edit function
-  //Could not work out edit function
-      editNote: function() {
-        alert("Edit funtion doesnot work");
-       
-      },
-  
+
+      deleteBtn.className = 'delete';
+      deleteBtn.innerHTML = 'Delete';
+      rxjs.fromEvent(deleteBtn, 'click')
+		  .subscribe(() => deleteNote(li)
+	    );
+
+
       
-      //delete function
-      deleteNote: function() {
-        this.parentNode.remove();
-      },
+    }
+    
+
+    
+    //delete function
+     function deleteNote(li) {
+      li.remove();
+    }
+
+
   
-        
-    };
-  
-    app.init();
-  
-  })();
