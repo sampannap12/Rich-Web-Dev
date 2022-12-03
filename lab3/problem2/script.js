201f39278784508c2941321fd93bf4b91d894342
sampannap12 <56804514+sampannap12@users.noreply.github.com>
2022-12-03 15:38:27 +0000

    adding input check in the script

--- lab3/problem2/script.js
+++ lab3/problem2/script.js
@@ -6,29 +6,44 @@ const { map,take} = rxjs.operators;
 var startbtn = document.getElementById("timerButton");
 rxjs.fromEvent(startbtn, "click").subscribe(() => time());
 
+
+
+
 function start(time){
 const durationInSeconds = time;
-
+console.log (time)
 interval(1000).pipe(take(durationInSeconds), map(count => durationInSeconds - count)).subscribe(countdown => {
-  const hrs  = (~~(countdown / 3600)).toString()
-  const mins = (~~((countdown % 3600) / 60)).toString()
-  const secs = (~~countdown % 60).toString()
-  // Display the result
+  const hrs  =  (Math.floor(countdown  / (3600))).toString()
+  const mins =  (Math.floor((countdown % (3600)) / ( 60))).toString()
+  const secs = (Math.floor(countdown % (60))) .toString()
+
   document.getElementById("display").innerHTML = `${hrs.padStart(2, '0')}:${mins.padStart(2, '0')}:${secs.padStart(2, '0')}`;
-  if(secs <=1)
-  {
-  document.getElementById("display").innerHTML = `${'00'}:${'00'}:${'00'}`;
-  }
-      
-   
- 
+  
+  
+
 })
 }
+// Display the result
+  
+
+
 
 function time() {
     var h = document.getElementById("hours").value;
     var m = document.getElementById("minutes").value;
     var s = document.getElementById("seconds").value;
-    time = h*3600+ m*60 + s;
-    start(time);
+
+    
+
+    if(h < 0 || m < 0 || s < 0) {
+        alert("An error has occured. Please enter a numerical value for the follwing columns\n\nHour: Less than 24\nMinute: Less than 60\nSecond: Less than 60");
+        startbtn.disabled = false;
+        return;
+    }
+    else {
+      time = (h*3600+(m*60+(s*1)));
+      
+      start(time);
+    }
+    
   }
